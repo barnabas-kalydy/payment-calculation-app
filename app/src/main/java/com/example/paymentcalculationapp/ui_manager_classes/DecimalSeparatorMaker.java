d90cbb2fc0ee46fc7f9bdf0a9d1e2818867ce024
Barnabas Kalydy <kalydybarnabas99@gmail.com>
2021-07-06 19:23:06 +0200

    Updated Decimal Separator Maker Class

--- app/src/main/java/com/example/paymentcalculationapp/ui_manager_classes/DecimalSeparatorMaker.java
+++ app/src/main/java/com/example/paymentcalculationapp/ui_manager_classes/DecimalSeparatorMaker.java
@@ -3,20 +3,15 @@ package com.example.paymentcalculationapp.ui_manager_classes;
 public class DecimalSeparatorMaker {
 
     public static String placeDecimalSeparators(String inputStringNumber) {
-        String reversedString = new StringBuilder(inputStringNumber).reverse().toString();
-        StringBuilder stringNumberWithDecimalPoints = new StringBuilder();
-        int counter = 1;
-        for (int i = 0; i < reversedString.length(); i++) {
-            stringNumberWithDecimalPoints.append(reversedString.charAt(i));
-            if (counter % 3 == 0 && i != reversedString.length() - 1)
-                stringNumberWithDecimalPoints.append(",");
-            counter++;
+        StringBuilder sb = new StringBuilder(inputStringNumber);
+        for (int i = inputStringNumber.length(); i > 0; i-=3) {
+            sb.insert(i, " ");
         }
-        return stringNumberWithDecimalPoints.reverse().toString();
+        return sb.toString();
     }
 
     public static String removeDecimalSeparators(String inputStringNumberWithDecimalSeparators) {
-        return inputStringNumberWithDecimalSeparators.replaceAll(",", "");
+        return inputStringNumberWithDecimalSeparators.replaceAll(" ", "");
     }
 
 }
